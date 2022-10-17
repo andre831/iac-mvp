57883c65e3261e029873ceeacdc9c20ea9f60fd4
Andre Andre Andre <andredupdm@gmail.com>
2022-10-16 21:14:34 -0300

    add front-end folder and create docker image

---
+++ front-end/src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p>
+      For a guide and recipes on how to configure / customize this project,<br />
+      check out the
+      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
+        >vue-cli documentation</a
+      >.
+    </p>
+  </div>
+</template>
+
+<style scoped lang="scss" />
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class HelloWorld extends Vue {
+  @Prop() private msg!: string;
+}
+</script>
